a9569be19d874c62836aa834ad80ddfc3eb00811
Arne Berner <arne.berner@student.fh-kiel.de>
2023-08-22 21:28:22 +0200

    changed the asyncstruct example to use assotypes

--- example/asyncourstructure/business/src/traits/repository.rs
+++ example/asyncourstructure/business/src/traits/repository.rs
@@ -1,7 +1,8 @@
 use crate::error::customerror::CustomError;
 
-pub trait Repository<Entity> {
-    async fn getsomething(&self) -> Result<Entity, CustomError>;
-    async fn postsomething(&mut self, entity: Entity) -> Result<i32, CustomError>;
+pub trait Repository {
+    type Entity;
+    async fn getsomething(&self) -> Result<Self::Entity, CustomError>;
+    async fn postsomething(&mut self, entity: Self::Entity) -> Result<i32, CustomError>;
     async fn deletesomething(&mut self, id: i32) -> Result<(), CustomError>;
 }
