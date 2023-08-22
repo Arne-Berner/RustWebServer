b0eec733061d7619bc59b1c785267f05849acb4e
Arne Berner <arne.berner@student.fh-kiel.de>
2023-08-22 22:28:10 +0200

    removed queries example

--- example/queries/src/queryhandler.rs
+++
@@ -1,9 +0,0 @@
-pub trait QueryHandler<ParamObject, Entity> {
-    fn handle(&self, param_obj: ParamObject) -> Result<Entity, CustomError>;
-}
-
-#[derive(Debug)]
-pub enum CustomError {
-    Internal,
-    External,
-}
