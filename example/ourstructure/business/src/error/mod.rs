c6c76b49b2c11417ad91321b6bdef056c353e216
Arne Berner <arne.berner@student.fh-kiel.de>
2023-08-22 12:35:40 +0200

    deleted weird idea file.

--- example/ourstructure/business/src/error/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod customerror;
-pub mod statuscode;
--- example/ourstructure/business/src/traits/command_handler.rs
+++
@@ -1,5 +0,0 @@
-use crate::error::customerror::CustomError;
-
-pub trait CommandHandler<ParamObject, Res> {
-    fn handle(&mut self, param_obj: ParamObject) -> Result<Res, CustomError>;
-}
