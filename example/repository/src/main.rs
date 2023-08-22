365fe82c0fe3429a8d1c9f6848399ca456ce21d5
Arne Berner <arne.berner@student.fh-kiel.de>
2023-08-22 21:27:58 +0200

    Updated to assotypes and the Readme

--- example/repository/src/main.rs
+++ example/repository/src/main.rs
@@ -1,62 +1,126 @@
-use compose::{get_do_something};
+use compose::get_do_something;
 
 fn main() {
     let do_something = get_do_something();
     do_something.run();
 }
 
-
 pub mod business {
-    pub struct Service<Rep: Repository> {
-        pub repository: Rep,
+    pub trait Repository {
+        type Entity;
+
+        fn getsomething(&self) -> Result<Self::Entity, CustomError>;
+        fn postsomething(&mut self, entity: Self::Entity) -> Result<i32, CustomError>;
+        fn deletesomething(&mut self, id: i32) -> Result<(), CustomError>;
     }
 
-    pub trait Repository {
-        fn getsomething(&self) -> &str;
+    pub struct Service<Rep> {
+        pub repository: Rep,
     }
 
     impl<Rep> Service<Rep>
     where
-        Rep: Repository,
+        Rep: Repository + std::fmt::Debug,
     {
-        pub fn run(&self) {
-            let name = &self.repository.getsomething();
-            print!("\nThe name is {} \n", name);
+        pub fn run(&self)
+        where
+            <Rep as Repository>::Entity: std::fmt::Debug,
+        {
+            let entity = &self.repository.getsomething();
+            match entity {
+                Ok(n) => print!("\nThe name is {:?} \n", n),
+                Err(_) => print!("\nSomething bad happened!"),
+            }
         }
         pub fn new(repository: Rep) -> Self {
             Service { repository }
         }
     }
+
+    #[derive(Debug, Copy, Clone)]
+    pub struct ExampleEntity{
+        pub name: &'static str,
+    }
+
+    pub struct CustomError;
 }
 
 pub mod data {
-    use crate::business::Repository;
+    use crate::business::{ExampleEntity, Repository};
 
-    pub struct RealRepo<'a> {
-        firstname: &'a str,
+
+    #[derive(Debug)]
+    pub struct RealRepo{
+        entity: ExampleEntity,
+
     }
 
-    impl Repository for RealRepo<'_> {
-        fn getsomething(&self) -> &str {
-            &self.firstname
+    impl RealRepo {
+        pub fn new() -> Self {
+            RealRepo {
+                entity: ExampleEntity { name: "Bob" },
+            }
         }
     }
 
-    impl RealRepo<'_> {
+    impl Repository for RealRepo {
+        type Entity = ExampleEntity;
+
+        fn getsomething(&self) -> Result<Self::Entity, crate::business::CustomError> {
+            Result::Ok(self.entity)
+        }
+
+        fn postsomething(
+            &mut self,
+            entity: Self::Entity,
+        ) -> Result<i32, crate::business::CustomError> {
+            self.entity = entity;
+            // this could be the ID of the entity
+            Result::Ok(1)
+        }
+
+        fn deletesomething(&mut self, _id: i32) -> Result<(), crate::business::CustomError> {
+            todo!()
+        }
+    }
+
+    // This showcases the typesafety.
+    // Since this does not implement Debug, it cannot be used in Service.
+    pub struct NonDebugRepo{
+        entity: ExampleEntity,
+    }
+
+    impl NonDebugRepo{
         pub fn new() -> Self {
-            RealRepo { firstname: "John" }
+            NonDebugRepo { entity: ExampleEntity { name: "None" } }
+        }
+    }
+
+    impl Repository for NonDebugRepo{
+        type Entity = ExampleEntity;
+
+        fn getsomething(&self) -> Result<Self::Entity, crate::business::CustomError> {
+            todo!()
+        }
+
+        fn postsomething(&mut self, entity: Self::Entity) -> Result<i32, crate::business::CustomError> {
+            todo!()
+        }
+
+        fn deletesomething(&mut self, id: i32) -> Result<(), crate::business::CustomError> {
+            todo!()
         }
     }
 }
 
 pub mod compose {
-    use crate::{
-        business::{Service},
-        data::RealRepo,
-    };
+    use crate::{business::Service, data::{RealRepo, NonDebugRepo}};
 
-
-        pub fn get_do_something() -> Service<RealRepo<'static>> {
-            Service::new(RealRepo::new())
-        }
+    pub fn get_do_something() -> Service<RealRepo> {
+        Service::new(RealRepo::new())
+    }
+    // This will not work, since nonDebug does not implement Debug
+    // pub fn get_do_something() -> Service<NonDebugRepo> {
+    //     Service::new(NonDebugRepo::new())
+    // }
 }
